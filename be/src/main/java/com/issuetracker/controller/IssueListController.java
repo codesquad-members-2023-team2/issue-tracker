8237be04b1b2b4ab4850a3d388c5a8653eacff63
ngw7617@naver.com <ngw7617@naver.com>
2023-05-15 17:25:50 +0900

    feat #12 : 라벨 등록 기능 구현

--- be/src/main/java/com/issuetracker/controller/IssueListController.java
+++ be/src/main/java/com/issuetracker/controller/IssueListController.java
@@ -1,5 +1,6 @@
 package com.issuetracker.controller;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,10 +60,24 @@ public class IssueListController {
     }
 
     @GetMapping("api/labels")
-    public LabelListDto labelList() {
+    public LabelListDto labelList() throws SQLException {
         //labelService.createLabel(new Label(null, "포로", "붉은 색", "푸른 색", "설명", false));
         //labelService.deleteLabelById(3);
         //labelService.updateLabel(new Label(2, "루크", "붉은 색", "푸른 색", "설명", false));
         return labelService.findAllLabels();
+    }
+
+    /**
+     * 라벨 등록 기능
+     * 예외처리 필요
+     * @param labelName
+     * @param backgroundColor
+     * @param fontColor
+     * @param description
+     */
+    @PostMapping("api/labels")
+    public String post(@RequestBody String labelName, @RequestBody String backgroundColor, @RequestBody String fontColor, @RequestBody String description) {
+        labelService.createLabel(new Label(labelName, backgroundColor, fontColor, description));
+        return "OK";
     }
 }
